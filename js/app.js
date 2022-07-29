91660944d6a425d99a006cf46ca5526d1042a687
Lucaponz91 <lucapo1991@msn.com>
2022-07-29 17:55:54 +0200

    add funzione in ciclo

--- js/app.js
+++ js/app.js
@@ -8,14 +8,33 @@ console.log(buttonElement)
 // IL BOTTONE AVVIERA' UN CICLO CHE GENERERA' 100 DIV
 buttonElement.addEventListener('click', function(){
     for (let i = 0; i < 100; i++) {
-    // creo il div
-    const div = document.createElement('div')
+    // // creo il div
+    // const div = document.createElement('div')
+    // // creo il contenuto del div (il numero della casella)
+    // div.innerHTML = i + 1
+    // // aggiungo la classe cella alle
+    // div.classList.add('cella')
+    // console.log(div)
+    // // inserisco il div creato nel div "grid"
+
+
+    // gridEl.append(div)
+    const cella = creaCelleDiv()
     // creo il contenuto del div (il numero della casella)
-    div.innerHTML = i + 1
-    console.log(div)
+    cella.innerHTML = i + 1
     // inserisco il div creato nel div "grid"
-    gridEl.append(div)
-    buttonElement.removeEventListener('click', function() {})
+    gridEl.append(cella)
     }   
 }
 )
+function creaCelleDiv () {
+    // creo il div
+    const div = document.createElement('div')
+    
+    // aggiungo la classe cella alle
+    div.classList.add('cella')
+    console.log(div)
+    return div
+    
+
+}
